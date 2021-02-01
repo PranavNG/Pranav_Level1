01c02a59f6751f5bb402b0a378fad9f1d6e10175
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-01 16:51:11 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,8 +2,8 @@
 #include<stdio.h>
 void main()
 {
-	int a,b,c;
-	scanf("%d %d", &a,&b);
-	c=a+b;
-	printf("%d + %d = %d", a,b,c);
+	int n1,n2,s;
+	scanf("%d %d", &n1,&n2);
+	s=n1+n2;
+	printf("%d + %d = %d",n1,n2,s);
 }
