fae41f05e1bd30337afcfc16d4a117cf9fa95f05
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-02 18:08:11 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,35 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+float h();
+float b();
+float d();
+
+void main()
+{
+	float H=h();
+	float B= b();
+	float D=d();
+	float vol=(H*D*B)/3+(D/B);
+	printf("The volume of the tromboloid is: %f", vol);
+}
+float h()
+{
+	float a;
+	printf("Enter the height: ");
+	scanf("%f", &a);
+	return a;
+}
+float b()
+{
+    float c;
+    printf("Enter the breadth: ");
+    scanf("%f", &c);
+    return c;
+}
+float d()
+{
+    float e;
+    printf("Enter the depth: ");
+    scanf("%f", &e);
+    return e;
+}
