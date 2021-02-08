c6fbdefe0cfacc18c23dee724cd7a5e38b05b83b
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-08 20:12:53 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,21 @@
-//WAP to find the sum of two fractions.
+#include<stdio.h>
+typedef struct frac
+{
+	int n,d;
+}frac;
+int main()
+{
+	int n1,n2,d1,d2;
+	printf("Enter numerator and denominator of first fraction: ");
+	scanf("%d %d", &n1,&d1);
+	printf("Enter numerator and denominator of second fraction: ");
+	scanf("%d %d", &n2,&d2);
+	frac f={n1,d1};
+	frac f1={n2,d2};
+	add(f,f1);
+}
+void add(frac f, frac f1)
+{
+	frac sum={f.n*f1.d+f1.n*f.d, f.d*f1.d};
+	printf("Sum is equal to: %d/%d", sum.n,sum.d);
+}
