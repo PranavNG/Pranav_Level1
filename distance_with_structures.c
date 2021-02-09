c5613df1bd1d319a0e5739ad5a643bb533304cdd
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-09 10:22:16 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,21 +1,33 @@
 //WAP to find the distance between two points using structures and 4 functions.
 #include<stdio.h>
 #include<math.h>
+void input();
+void calc(float, float ,float ,float);
+void display(float);
 struct points
 {
 	int x1,y1,x2,y2;
 };
 int main()
 {
-	float dist,s1,s2;
+input();
+	return 0;
+}
+void input()
+{
 	struct points p;
 	printf("Enter values of x1,y1: ");
-	scanf("%d %d", &p.x1,&p.y1);
-	printf("Enter values of x2, y2: ");
-	scanf("%d %d", &p.x2, &p.y2);
-	s1=p.x1-p.x2;
-	s2=p.y1-p.y2;
-	dist=sqrt(s1*s1+s2*s2);
-	printf("Distance between points is: %f", dist);
-	return 0;
+	scanf("%d %d ", &p.x1,&p.y1);
+	printf("enter values of x2,y2: ");
+	scanf("%d %d", &p.x2,&p.y2);
+	calc(p.x1,p.y1,p.x2,p.y2);
+}
+void calc(float a,float b,float c,float d)
+{
+	float dist=sqrt((a-c)*(a-c)+(b-d)*(b-d));
+	display(dist);
+}
+void display(float d)
+{
+	printf("The distance is %f", d);
 }
