51cdbc9eab6d2dbffb5d97d60536351bd4e6d41c
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-19 21:09:21 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -10,6 +10,14 @@ frac input()
 	scanf("%d %d", &f.n,&f.d);
 	return f; 
 }
+int GCD(int x, int y)
+{
+    if (y == 0) {
+        return x;
+    }
+ 
+    return GCD(y, x % y);
+}
 frac add(struct frac f,struct frac f1)
 {
 	int gcd= GCD(f1.d,f2.d);
@@ -20,14 +28,6 @@ frac add(struct frac f,struct frac f1)
 void display(struct frac sum)
 {
 	printf("The sum of the fractions is, %d/%d", sum.n,sum.d);
-}
-int GCD(int x, int y)
-{
-    if (y == 0) {
-        return x;
-    }
- 
-    return GCD(y, x % y);
 }
 int main()
 {
