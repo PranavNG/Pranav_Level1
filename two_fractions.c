cfe0a2245107fc538aca71841a37de92392d940f
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-19 13:48:22 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,33 +1,30 @@
 #include<stdio.h>
-void input();
-void add();
-void display();
 typedef struct frac
 {
 	int n,d;
 }frac;
-void input()
+frac input()
 {
-	int n1,n2,d1,d2;
+	frac f;
 	printf("Enter numerator and denominator of first fraction: ");
-	scanf("%d %d", &n1,&d1);
-	printf("Enter numerator and denominator of second fraction: ");
-	scanf("%d %d", &n2,&d2);
-	frac f={n1,d1};
-	frac f1={n2,d2};
-	add(f,f1);
+	scanf("%d %d", &f.n,&f.d);
+	return f; 
 }
-void add(frac f, frac f1)
+frac add(struct frac f,struct frac f1)
 {
 	frac sum={f.n*f1.d+f1.n*f.d, f.d*f1.d};
-	display(sum);
+	return sum;
 }
-void display(frac sum)
+void display(struct frac sum)
 {
 	printf("The sum of the fractions is, %d/%d", sum.n,sum.d);
 }
 int main()
 {
-	input();
+	frac f1,f2,sum;
+	f1=input();
+	f2=input();
+	sum=add(f1,f2);
+	display(sum);
 	return 0;
 }
