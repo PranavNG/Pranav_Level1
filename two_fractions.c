037435c9546afffdc2ee9b881aab3673771e55fa
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-02-20 23:03:18 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -21,12 +21,16 @@ int GCD(int x, int y)
 frac add(struct frac f1,struct frac f2)
 {
 	int gcd= GCD(f1.d,f2.d);
-	frac sum={(f1.n*f2.d+f2.n*f1.d)/gcd, (f1.d*f2.d)/gcd};
-	return sum;
+	int f_den=(f1.d*f2.d)/gcd;
+	int f_num = (f1.n)*(f_den/f1.d) + (f2.n)*(f_den/f2.d); 
+	frac sum={f_num,f_den};
+	int new_gcd=GCD(sum.n,sum.d);
+	frac f_sum={f_num/new_gcd, f_den/new_gcd};
+	return f_sum;
 }
-void display(struct frac sum)
+void display(struct frac f_sum)
 {
-	printf("The sum of the fractions is, %d/%d", sum.n,sum.d);
+	printf("The sum of the fractions is, %d/%d", f_sum.n,f_sum.d);
 }
 int main()
 {
@@ -34,6 +38,7 @@ int main()
 	f1=input();
 	f2=input();
 	sum=add(f1,f2);
+	
 	display(sum);
 	return 0;
 }
