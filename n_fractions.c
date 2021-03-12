ba8af5f07642225f94170549ae0b7f3d671451b9
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-03-12 14:48:19 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -2,14 +2,28 @@
 struct Frac{
 	int n,d;
 };
+int number_of_frac()
+{
+    int nf;
+    printf("Enter number of fractions: ");
+	scanf("%d",&nf);
+	return nf;
+}
 struct Frac input()
 {
-	struct Frac f;
+    struct Frac f;
 	printf("Enter numerator ");
 	scanf("%d",&f.n);
 	printf("Enter denominator ");
 	scanf("%d",&f.d);
 	return f;
+}
+void input_n_fractions(int n, struct Frac array[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        array[i]=input();
+    }
 }
 int GCD(int x, int y)
 {
@@ -17,15 +31,22 @@ int GCD(int x, int y)
 		return x;
 	return GCD(y,x%y);
 }
-struct Frac add(struct Frac f1,struct Frac f2)
+struct Frac add(struct Frac f1, struct Frac f2)
 {
     struct Frac temp;
 	temp.d=(f1.d*f2.d);
-	temp.n= (f1.n * f2.d) + (f2.n * f1.d); 
-	int gcd=GCD(temp.n,temp.d);
-	temp.n/=gcd;
-	temp.d/=gcd;
+	temp.n= (f1.n * f2.d) + (f1.n * f2.d); 
+    int gcd=GCD(temp.n,temp.d);
+    temp.n/=gcd;
+    temp.d/=gcd;
 	return temp;
+}
+void add_n_fractions(int n, struct Frac arr[n])
+{
+    for(int i=0;i<n;i++)
+	{
+	    arr[i]=add(arr[i],arr[i+1]);
+	}
 }
 void display(struct Frac sum)
 {
@@ -33,21 +54,10 @@ void display(struct Frac sum)
 }
 int main()
 {
-	int nf;
-	struct Frac sum;
-	sum.n=0;
-	sum.d=1;
-	printf("Enter number of fractions: ");
-	scanf("%d",&nf);
+	int nf= number_of_frac();
 	struct Frac f[nf];
-	for(int i=0;i<nf;i++)
-	{
-		f[i]=input();
-	}
-	for(int i=0;i<nf;i++)
-	{
-		sum=add(f[i],sum);
-	}
-	display(sum);
+	input_n_fractions(nf,f);
+	add_n_fractions(nf,f);
+	display(f);
 	return 0;
 }
