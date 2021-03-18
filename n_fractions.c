b7aac2f9b1015e7b96e8e633ddf4e2b1c33624ac
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-03-18 16:25:03 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -31,41 +31,45 @@ int GCD(int x, int y)
 		return x;
 	return GCD(y,x%y);
 }
-struct Frac add(struct Frac f1, struct Frac f2)
+struct Frac add(struct Frac f1,struct Frac f2)
 {
     struct Frac temp;
 	temp.d=(f1.d*f2.d);
-	temp.n= (f1.n * f2.d) + (f1.n * f2.d); 
-    int gcd=GCD(temp.n,temp.d);
-    temp.n/=gcd;
-    temp.d/=gcd;
+	temp.n= (f1.n * f2.d) + (f2.n * f1.d); 
+	int gcd=GCD(temp.n,temp.d);
+	temp.n/=gcd;
+	temp.d/=gcd;
 	return temp;
-}
-void add_n_fractions(int n, struct Frac arr[n])
+} 
+struct Frac add_n_fractions(int n, struct Frac array[n])
 {
+    struct Frac sum;
+    sum.n=0;
+    sum.d=1;
     for(int i=0;i<n;i++)
 	{
-	    arr[i]=add(arr[i],arr[i+1]);
+	    sum=add(sum,array[i]);
 	}
+	return sum;
 }
-void display(struct Frac sum)
+void display(int n, struct Frac lf[n],struct Frac sum)
 {
-	printf("%d/%d", sum.n,sum.d);
-}
-void display_n(int n,struct Frac arr[n])
-{
-    printf("the sum of fractions is ");
+    
+    printf("The sum of fractions ");
     for(int i=0;i<n;i++)
     {
-        display(arr[n]);
+        printf("%d/%d, ",lf[i].n,lf[i].d);
     }
+    
+    printf(" is %d/%d", sum.n, sum.d);
 }
 int main()
 {
-	int nf= number_of_frac();
+	int nf=number_of_frac();
 	struct Frac f[nf];
 	input_n_fractions(nf,f);
-	add_n_fractions(nf,f);
-	display_n(nf,f);
+	struct Frac f_sum;
+	f_sum=add_n_fractions(nf,f);
+	display(nf,f,f_sum);
 	return 0;
 }
