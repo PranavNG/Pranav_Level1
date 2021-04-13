3c7e8d38b084ffe2b3cde1826f235d6cc44fa13a
PranavNG <65937732+PranavNG@users.noreply.github.com>
2021-04-13 12:10:35 +0530

    Update Activity13.c

--- Activity13.c
+++ Activity13.c
@@ -1,31 +1,28 @@
 //Actvity 13
 #include <stdio.h>
 #include <math.h>
-
 struct rectangle
 {
     float x1, y1, x2, y2, x3, y3, area;
 };
 typedef struct rectangle rec;
-
 rec input_one()
 {
     rec rect;
     scanf("%f %f %f %f %f %f", &rect.x1, &rect.y1, &rect.x2, &rect.y2, &rect.x3, &rect.y3);
     return rect;
 }
-
 void input_n(int n, rec rectangles[n])
 {
     for (int i = 0; i < n; i++)
+    {
         rectangles[i] = input_one();
+    }
 }
-
 float dist(float x1, float y1, float x2, float y2)
 {
     return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 }
-
 float calc_area(float l, float b)
 {
     return l * b;
@@ -35,7 +32,6 @@ void compute_one(rec *rect)
     float length = dist(rect->x1, rect->y1, rect->x2, rect->y2);
     float breadth = dist(rect->x2, rect->y2, rect->x3, rect->y3);
     float height = dist(rect->x1, rect->y1, rect->x3, rect->y3);
-
     if (length > breadth && length > height)
         rect->area = calc_area(breadth, height);
     else if (breadth > length && breadth > height)
@@ -43,24 +39,24 @@ void compute_one(rec *rect)
     else
         rect->area = calc_area(length, breadth);
 }
-
 void compute_n(int n, rec rectangles[n])
 {
     for (int i = 0; i < n; i++)
+    {
         compute_one(&rectangles[i]);
+    }
 }
-
 void output_one(rec rect)
 {
     printf("Area of rect with vertices (%f,%f), (%f,%f), (%f,%f) is %.2f \n",rect.x1, rect.y1, rect.x2, rect.y2, rect.x3, rect.y3, rect.area);
 }
-
 void output_n(int n, rec rectangles[n])
 {
     for (int i = 0; i < n; i++)
+    {
         output_one(rectangles[i]);
+    }
 }
-
 int main()
 {
     int n;
